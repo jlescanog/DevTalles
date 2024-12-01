8d5095c01f585aaeba931050b48acc96718d817c
jlescano-mobup <jlescano@mobuptacna.com>
2024-11-30 23:41:53 -0500

    Tarea de tabla pendiente

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,2 +1,15 @@
 
-console.log('Hola Mundo')
+let inicio = [1,2,3,4,5];
+
+for (let i = 0; i < inicio.length ; i++) {
+
+    console.log(inicio[i])
+
+    if (inicio[i] < 5) {
+        console.log(inicio[i] * 2);
+    
+    }
+
+    
+    
+}
