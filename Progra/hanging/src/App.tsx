770dc7ec807f650f9f671f265588ccde28058135
MyAorus-Jhair <jlescanoguevara@gmail.com>
2024-12-21 00:46:21 -0500

    se agrega funcionalidad al boton

--- Progra/hanging/src/App.tsx
+++ Progra/hanging/src/App.tsx
@@ -1,26 +1,36 @@
+import { useState } from 'react';
 import {letters} from './helpers/letters';
-import { HangImage } from './components/HangImage';
+import { HangImage } from './components/hangImage';
 
 import './App.css';
 
+
 function App() {
+
+  const [ attempts, setAttemps ] = useState(0);
+
+  const checkLetter = (letter: string ) => {
+    console.log(letter);
+    setAttemps( Math.min ( attempts + 1, 9) );
+  }
 
   return (
   <div className="App">
     
       {/* Imagenes */}
-      <HangImage imageNumber={ 3 } />
+      <HangImage imageNumber={ attempts } />
 
       {/* Palabra oculta */}
       <h3>__________________</h3>
 
       {/* Contador de Intentos */}
-      <h3>Intentos: 0</h3>
+      <h3>Intentos: {attempts} </h3>
 
       {/* Botones de letras */}
       {
         letters.map((letter) => (
-          <button 
+          <button
+            onClick={ () => checkLetter(letter) }
             key={letter}>
               {letter}
           </button>
